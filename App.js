52e7598ed76a96c26d7668cb15223aced987d965
shine <shine@turingcat.com>
2019-11-20 18:10:50 +0800

    hello RN MusicPlayer!

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hello, RN MusicPlayer !</Text>
+    </View>
+  );
+}
+export default App;
